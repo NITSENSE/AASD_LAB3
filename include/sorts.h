e638c0e0c24ad588890cec08fc4c36af5064b9c5
NITSENSE KO <volodia.kostsoff@yandex.ru>
2024-03-26 10:19:27 +0400

    binary_insertion_sort

--- include/sorts.h
+++ include/sorts.h
@@ -7,9 +7,9 @@ using namespace std;
 
 template<typename T>
 void SelectionSort(vector<T>& sort) {
-	for (size_t i = 0; i < sort.size(); ++i) {
-		size_t temp = i;
-		for (size_t j = i; j < sort.size(); ++j) {
+	for (int i = 0; i < sort.size(); ++i) {
+		int temp = i;
+		for (int j = i; j < sort.size(); ++j) {
 			if (sort[temp] > sort[j])
 				temp = j;
 		}
@@ -20,8 +20,8 @@ void SelectionSort(vector<T>& sort) {
 
 template<typename T>
 void BubbleSort(vector<T>& sort) {
-	for (size_t i = sort.size() - 1; i > 0; --i) {
-		for (size_t j = 1; j <= i; ++j) {
+	for (int i = sort.size() - 1; i > 0; --i) {
+		for (int j = 1; j <= i; ++j) {
 			if (sort[j] < sort[j - 1])
 				swap(sort[j], sort[j - 1]);
 		}
@@ -31,8 +31,30 @@ void BubbleSort(vector<T>& sort) {
 
 template<typename T>
 void InsertionSort(vector<T>& sort) {
-	for (size_t i = 1; i < sort.size(); ++i) {
-		for (size_t j = i; j >= 1 && sort[j - 1] > sort[j]; j--)
+	for (int i = 1; i < sort.size(); ++i) {
+		for (int j = i; j >= 1 && sort[j - 1] > sort[j]; j--)
 			swap(sort[j], sort[j - 1]);
 	}
 }
+
+template<typename T>
+void BinaryInsertionSort(vector<T>& sort) {
+	for (int i = 1; i < sort.size(); i++) {
+		int lower = 0;
+		int upper = i - 1;
+		T key = sort[i];
+		while (lower <= upper) {
+			int mid = (lower + upper) / 2;
+
+			if (sort[mid] < key) {
+				lower = mid + 1;
+			}
+			else
+				upper = mid - 1;
+		}
+
+		for (int j = i; j > lower; j--)
+			swap(sort[j], sort[j - 1]);
+	}
+}
+
